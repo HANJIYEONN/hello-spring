c890b5f0855a3d282841aae07bc6ffd8a91630fa
HANJIYEON <onedelay1@naver.com>
2021-09-15 23:34:29 +0900

    스프링 DB 접근 기술 / JPA(1)

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Member {
 
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //데이터를 구분하기 위한 시스템 id 값 (시퀀스 넘버 같은것.)
     private String name; // 실제로 사용되는  id
 
